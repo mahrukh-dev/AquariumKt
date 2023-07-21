faf737461443e6e3381e4b80246c49236f2e5af5
MahRukh <mahrukhdev@gmail.com>
2023-07-21 12:08:34 +0500

    practice Questions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -24,3 +24,31 @@ fun nullability(){
     var evenMoreFish: List<String>? = null
     var definatelyFish: List<String?>? = null
 }
+
+fun PracticeBasicOperators(){
+    var fish = 2
+    fish.plus(71).plus(233).minus(13).div(30).plus(1)
+}
+
+fun PracticeVariables(){
+    var rainbowColor: String = "blue"
+    rainbowColor = "red"
+    val blackColor: String = "black"
+}
+
+fun PracticeNullability(){
+    var greenColor = null
+    var blueColor: Int? = null
+}
+
+fun PracticeNullabilityLists(){
+    var listOne: List<String?> = listOf(null, null)
+    var listTwo = listOf<String?>(null, null)
+    var listThree: List<String?>? = null
+}
+
+fun PracticeNullCheck(){
+    var nullTest: Int? = null
+    var returnval = nullTest?.plus(1) ?: 0
+    println(returnval)
+}
