62b603f5ed1ade9945bfb3af23cbccdabb9af76c
MahRukh <mahrukhdev@gmail.com>
2023-07-24 15:10:12 +0500

    generic in

---
+++ src/main/kotlin/GenericIn.kt
@@ -0,0 +1,11 @@
+class Consumer<in T> {
+    fun consume(item: T) {
+        println("Consuming: $item")
+    }
+}
+
+fun main() {
+    val consumer: Consumer<Number> = Consumer()
+    consumer.consume(42) // OK, since Consumer is contravariant
+    consumer.consume(3.14) // OK, since Consumer is contravariant
+}
